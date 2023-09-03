f234aaa31c9d32a911de4f44617904247d8146eb
Ettienne Smit <devel@assemblage24.com>
2023-09-03 12:53:57 +0200

    Basic menu loop created

--- app.py
+++ app.py
@@ -11,6 +11,29 @@ Select an option:
 'd' - Delete book from database
 'q' - Quit
 
-Your choice: 
-"""
+Your choice: """
 
+
+def menu():
+    user_choice = ' '
+    while user_choice != 'q':
+        user_choice = input(USER_CHOICE)
+        
+        if user_choice == 'a':
+            pass
+        elif user_choice == 'l':
+            pass
+        elif user_choice == 'm':
+            pass
+        elif user_choice == 'd':
+            pass
+        elif user_choice == 'q':
+            print("Programme terminated")
+            break
+        else:
+            print("That choice doesn't exist, try again.")
+
+
+
+
+menu()
