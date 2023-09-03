3aed417f5b4294aaadc0ba6f57dd35327122ac88
Ettienne Smit <devel@assemblage24.com>
2023-09-03 12:37:43 +0200

    Description of application and initial variables created

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+a Books database app with menu
+"""
+
+USER_CHOICE = """
+
+Select an option:
+'a' - Add a book to database
+'l' - List all books in database
+'m' - Mark book as read in database
+'d' - Delete book from database
+'q' - Quit
+
+Your choice: 
+"""
+
---
+++ config/database.py
@@ -0,0 +1,5 @@
+"""
+Concerened with storing, retrieving, editing and deleting books from the 'in memory' database
+"""
+
+books = []
