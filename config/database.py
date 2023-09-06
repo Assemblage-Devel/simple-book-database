91a5a27e1e43f775df0a8241206ba9ccce9ea57e
Ettienne Smit <devel@assemblage24.com>
2023-09-06 18:37:35 +0200

    Add Context-manager for db connection

--- config/database.py
+++ config/database.py
@@ -1,45 +1,39 @@
-import sqlite3
+from .db_connection import DatabaseConnection
 
 """
 Concerened with storing, retrieving, editing and deleting books from sqlite database
 
 """
-
+host = "data.db"
 
 def create_book_table():
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection(host) as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)")
-    
-    connection.commit()
-    connection.close()
+        cursor.execute("CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)")
+        
 
 
 def add_book(name, author):
     """
     function adds provided book entry to database
     """
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
-
-    cursor.execute("INSERT INTO books VALUES(?, ?, 0)",(name, author))
-    
-    connection.commit()
-    connection.close()
+    with DatabaseConnection(host) as connection:
+        cursor = connection.cursor()
+        
+        cursor.execute("INSERT INTO books VALUES(?, ?, 0)",(name, author))
 
 
 def get_books():
     """
     function retrieves entries from database
     """
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection(host) as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM books")
-    # books = cursor.fetchall() # list of tupples [(name,author,read),(..),(..)]
-    books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
-    connection.close()
+        cursor.execute("SELECT * FROM books")
+        # books = cursor.fetchall() # list of tupples [(name,author,read),(..),(..)]
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
 
     return books
     
@@ -48,26 +42,20 @@ def mark_book(name, author):
     """
     function marks provided entry in database to read: True
     """
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection(host) as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("UPDATE books SET read=1 WHERE name=? AND author=?", (name, author))
-    
-    connection.commit()
-    connection.close()
+        cursor.execute("UPDATE books SET read=1 WHERE name=? AND author=?", (name, author))
 
 
 def del_book(name, author):
     """
     function removes provided entry from database
     """
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
+    with DatabaseConnection(host) as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("DELETE FROM books WHERE name=? AND author=?", (name, author))
-    
-    connection.commit()
-    connection.close()
+        cursor.execute("DELETE FROM books WHERE name=? AND author=?", (name, author))
 
 
 
