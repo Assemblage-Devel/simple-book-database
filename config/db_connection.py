d63efe42045fb5f123a6ab7633d65ac62b06c058
Ettienne Smit <devel@assemblage24.com>
2023-09-06 18:39:54 +0200

    complete v1

---
+++ config/db_connection.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Context manager
+
+class DatabaseConnection:
+    def __init__(self, host):
+        self.connection = None
+        self.host = host
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.host)
+        return self.connection
+
+
+    def __exit__(self, exc_type, exc_value, tb):
+        self.connection.commit()
+        self.connection.close()
+
